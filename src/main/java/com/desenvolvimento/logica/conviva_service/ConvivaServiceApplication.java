17af867563d7fb368dd097af2067c12de1f0e8e5
rhuan <rhuan.resende@hotmail.com.br>
2026-08-08 23:14:32 -0300

    First commit

---
+++ src/main/java/com/desenvolvimento/logica/conviva_service/ConvivaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.logica.conviva_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvivaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvivaServiceApplication.class, args);
+	}
+
+}
